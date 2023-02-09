c0db06b5142306fac8699a37ba8d5b2795da963c
MoonStar <moonsung0331@gmail.com>
2023-02-10 00:32:21 +0900

    JPA Auditing으로 생성시간/수정시간 자동화 구현

---
+++ src/main/java/com/example/pharmacy/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.pharmacy;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 엔티티의 공통 매핑 정보
+@EntityListeners(AuditingEntityListener.class) // 해당 클래스에 auditing 기능을 포함
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
